c6f14281b30ef03ef292fe299b3e5787440e545a
zhaojiahang <751726931@qq.com>
2019-02-21 21:34:15 +0800

    新模型

---
+++ Animation/Model.h
@@ -0,0 +1,17 @@
+//
+//  Model.h
+//  Animation
+//
+//  Created by 赵加航 on 2019/2/21.
+//  Copyright © 2019 zhaojiahang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Model : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Animation/Model.m
@@ -0,0 +1,13 @@
+//
+//  Model.m
+//  Animation
+//
+//  Created by 赵加航 on 2019/2/21.
+//  Copyright © 2019 zhaojiahang. All rights reserved.
+//
+
+#import "Model.h"
+
+@implementation Model
+
+@end
